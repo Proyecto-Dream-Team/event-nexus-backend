6c2b13fffe82e0491fef92706747ef19eee175df
AdrianPerez0306 <perezadrian.m@hotmail.com>
2025-04-04 14:55:24 -0300

    Archivos de RepportModule y DIrectiveModule para completar la estructura

---
+++ src/main/kotlin/ar/edu/unsam/proyecto_de_sofware/event_nexus/model/modules/base/directive/DirectiveModule.kt
@@ -0,0 +1,6 @@
+package ar.edu.unsam.proyecto_de_sofware.event_nexus.model.modules.base.directive
+
+import ar.edu.unsam.proyecto_de_sofware.event_nexus.model.modules.common.AppModule
+
+class DirectiveModule:AppModule {
+}
