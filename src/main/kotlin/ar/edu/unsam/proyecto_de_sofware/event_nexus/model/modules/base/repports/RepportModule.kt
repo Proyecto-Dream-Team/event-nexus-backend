82bab8b1653bfd2256f3eea6834082ec13272283
Diaz Matias Hernan <diaz.matiash@gmail.com>
2025-04-07 23:44:48 -0300

    se crean 4 modulos para que el front los pueda mostrar en el home

--- src/main/kotlin/ar/edu/unsam/proyecto_de_sofware/event_nexus/model/modules/base/repports/RepportModule.kt
+++ src/main/kotlin/ar/edu/unsam/proyecto_de_sofware/event_nexus/model/modules/base/repports/RepportModule.kt
@@ -3,4 +3,10 @@ package ar.edu.unsam.proyecto_de_sofware.event_nexus.model.modules.base.repports
 import ar.edu.unsam.proyecto_de_sofware.event_nexus.model.modules.common.AppModule
 
 class RepportModule: AppModule {
+    val name: String = "Reporte de faltantes"
+    val img: String = ""
+
+    fun shortageNotification(){}
+
+    fun replacementNotification(){}
 }
