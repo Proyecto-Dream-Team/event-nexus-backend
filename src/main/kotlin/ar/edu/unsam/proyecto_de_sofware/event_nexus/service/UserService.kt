43b0be9867ab9587d5111c1577083e13656322d9
AdrianPerez0306 <perezadrian.m@hotmail.com>
2025-04-11 03:02:47 -0300

    User service con valor Int mapeado a Long

--- src/main/kotlin/ar/edu/unsam/proyecto_de_sofware/event_nexus/service/UserService.kt
+++ src/main/kotlin/ar/edu/unsam/proyecto_de_sofware/event_nexus/service/UserService.kt
@@ -1,14 +1,13 @@
 package ar.edu.unsam.proyecto_de_sofware.event_nexus.service
 
-import ar.edu.unsam.proyecto_de_sofware.event_nexus.model.User
-import ar.edu.unsam.proyecto_de_sofware.event_nexus.model.UserNew
+import ar.edu.unsam.proyecto_de_sofware.event_nexus.model.Employee
 import ar.edu.unsam.proyecto_de_sofware.event_nexus.repository.UserRepository
 import org.springframework.stereotype.Service
 
 @Service
 class UserService(val repoUser: UserRepository) {
 
-    fun getUser(id : Int): UserNew {
+    fun getUser(id : Int): Employee {
         val user = repoUser.findById(id.toLong())
         return user.get()
     }
