485476cbe350258ba3105e5cf62768a190468e81
ValentinPg <valentinpugliesew@outlook.com>
2025-04-25 17:28:34 -0300

    se agregan ids de los participantes y el creatorId

--- src/main/kotlin/ar/edu/unsam/proyecto_de_sofware/event_nexus/dto/EventDTO.kt
+++ src/main/kotlin/ar/edu/unsam/proyecto_de_sofware/event_nexus/dto/EventDTO.kt
@@ -15,23 +15,26 @@ data class ShowEventDTO(
     val id: Long?,
     val creatorName: String,
     val creatorImage: String,
+    val creatorId: Long?,
     val dateFinished: LocalDateTime,
     val title: String,
     val description: String,
     val isActive: Boolean,
-    val numberOfParticipants: Int
+    val numberOfParticipants: Int,
+    val participantsIds: List<Long?>
 )
 
 fun Event.showEventDTO(): ShowEventDTO {
     return ShowEventDTO(
         id = id,
-        creatorName = creator.name + " "+ creator.lastname,
+        creatorName = creator.name + " " + creator.lastname,
         creatorImage = creator.image,
+        creatorId = creator.id,
         dateFinished = dateFinished,
         title = title,
         description = description,
         isActive = isPending(),
-        numberOfParticipants = participants.size + 1
-
+        numberOfParticipants = participants.size + 1,
+        participantsIds = participants.map { it.id }
     )
 }
