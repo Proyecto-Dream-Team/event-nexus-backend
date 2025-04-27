d29e1c1465d931c136bf408957a3aef7a5c7b7c8
Diaz Matias Hernan <diaz.matiash@gmail.com>
2025-04-27 00:26:21 -0300

    se eliminan eventos

--- src/main/kotlin/ar/edu/unsam/proyecto_de_sofware/event_nexus/exceptions/ExceptionMock.kt
+++ src/main/kotlin/ar/edu/unsam/proyecto_de_sofware/event_nexus/exceptions/ExceptionMock.kt
@@ -3,7 +3,7 @@ package ar.edu.unsam.proyecto_de_sofware.event_nexus.exceptions
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.ResponseStatus
 
-@ResponseStatus(HttpStatus.NOT_FOUND)
+@ResponseStatus(HttpStatus.NOT_FOUND) //TODO aca no tendria que ser un NOT_FOUND
 class BusinessException(override val message:String): Exception(message)
 
 @ResponseStatus(HttpStatus.NOT_MODIFIED)
