3b5fea0def58971434d1eab7ed28cb106b83b87d
Diaz Matias Hernan <diaz.matiash@gmail.com>
2025-05-14 16:37:43 -0300

    se cambia mail porque el otro no se porque no anda

--- src/main/kotlin/ar/edu/unsam/proyecto_de_sofware/event_nexus/service/EmailService.kt
+++ src/main/kotlin/ar/edu/unsam/proyecto_de_sofware/event_nexus/service/EmailService.kt
@@ -1,5 +1,6 @@
 package ar.edu.unsam.proyecto_de_sofware.event_nexus.service
 
+import ar.edu.unsam.proyecto_de_sofware.event_nexus.exceptions.BusinessException
 import org.springframework.mail.javamail.JavaMailSender
 import org.springframework.mail.javamail.MimeMessageHelper
 import org.springframework.stereotype.Service
@@ -10,14 +11,21 @@ class EmailService(private val mailSender: JavaMailSender) {
         val message = mailSender.createMimeMessage()
         val helper = MimeMessageHelper(message, true)
 
-        helper.setTo(to)
-        helper.setSubject(subject)
-        helper.setText(content, true) // true si es HTML
-
         try{
-            mailSender.send(message)
+            helper.setTo(to)
+            helper.setSubject(subject)
+            helper.setText(content, true) // true si es HTML
+            for (intento in 1..3) {
+                try {
+                    mailSender.send(message)
+                    return
+                } catch (e: Exception) {
+                    if (intento == 3) throw BusinessException("Error al enviar el correo: ${e.message}")
+                    Thread.sleep(1000)
+                }
+            }
         }catch (e: Exception){
-            throw Exception("Falla en el envio del mail")
+            throw BusinessException("Falla en el envio del mail")
         }
     }
 }
