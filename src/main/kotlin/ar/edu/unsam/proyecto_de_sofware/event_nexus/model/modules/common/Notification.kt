24dd49b5998bc57e5787d7274d58d7e78a27cd46
AdrianPerez0306 <perezadrian.m@hotmail.com>
2025-05-16 15:44:49 -0300

    Se comenta un initializer que no servia

--- src/main/kotlin/ar/edu/unsam/proyecto_de_sofware/event_nexus/model/modules/common/Notification.kt
+++ src/main/kotlin/ar/edu/unsam/proyecto_de_sofware/event_nexus/model/modules/common/Notification.kt
@@ -1,4 +1,11 @@
 package ar.edu.unsam.proyecto_de_sofware.event_nexus.model.modules.common
 
-class Notification {
-}
+
+data class Notification(
+    var id:Long? = null,
+    var from:String,
+    var date:String,
+    var module:String,
+    var readed:Boolean = false,
+    var type:String
+)
